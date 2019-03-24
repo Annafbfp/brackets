df26ce4b648efa3b7ab2b00fa1e5a72c82c4831a
annafbfp <adrien@tut.by>
2019-03-24 23:32:45 +0300

    feat: add function for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+    
+    var arr = [];
+    if (str.length % 2 !== 0) return false;
+        for (var k = 0; k < str.length; k++){
+            outer: for (var i = 0; i < bracketsConfig.length; i++) {
+                if (str[k] == bracketsConfig[i][0]){   
+                    arr.push(str[k]);
+                    break outer;
+                }
+            }
+            
+        if (arr.length == 0){
+                    
+            return false;
+        }
+        for (var m = 0; m < bracketsConfig.length; m++) {
+            if (str[k] == bracketsConfig[m][1]){
+                 
+                var top = arr.pop();
+                
+                if (top !== bracketsConfig[m][0] || str[k] !== bracketsConfig[m][1]){
+                    return false;  
+                }     
+            }
+        }
+        
+        }       
+        if (arr.length == 0){
+            return true;
+        }
+
 }
